abd2758575d25377718ec67a790d8f958b2bb243
aidenwhite27 <aidenwhite27@gmail.com>
2023-01-10 19:10:00 -0800

    database connection

--- php/db_connect.php
+++ php/db_connect.php
@@ -1,10 +1,10 @@
 <?php
 
 // variables for connection
-$localhost = "localhost";
-$user = "root";
-$password = "Baseball1";
-$database = "watchlistapp";
+$localhost = "us-cdbr-east-06.cleardb.net";
+$user = "b97078e238fb66:";
+$password = "9e3c8562";
+$database = "heroku_5d3cbded229dc48";
 
 // create a database connection instance
 $mysqli = new mysqli($localhost, $user, $password, $database);
