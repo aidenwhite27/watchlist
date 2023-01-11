c40ef97255b088e6b4a116c1d4908e0f425937ab
aidenwhite27 <aidenwhite27@gmail.com>
2023-01-10 19:40:36 -0800

    Fixed database connection

--- php/db_connect.php
+++ php/db_connect.php
@@ -2,7 +2,7 @@
 
 // variables for connection
 $localhost = "us-cdbr-east-06.cleardb.net";
-$user = "b97078e238fb66:";
+$user = "b97078e238fb66";
 $password = "9e3c8562";
 $database = "heroku_5d3cbded229dc48";
 
